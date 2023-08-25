9d39180de2eb4a0f272f2459164fed3dd38bd1d4
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-08-26 02:26:57 +0530

    Add files via upload

--- src/item.js
+++ src/item.js
@@ -1,10 +1,186 @@
 import veg1 from "./images/veg1.jpg";
+import v2 from "./images/v2.jpg"
+import v3 from "./images/v2.jpg"
+import v4 from "./images/v2.jpg"
+import v5 from "./images/v2.jpg"
+import v6 from "./images/v2.jpg"
+import v7 from "./images/v2.jpg"
+import v8 from "./images/v2.jpg"
+import v9 from "./images/v2.jpg"
+import v10 from "./images/v2.jpg"
+import v11 from "./images/v2.jpg"
+import v12 from "./images/v2.jpg"
+import v13 from "./images/v2.jpg"
+import v14 from "./images/v2.jpg"
+import v15 from "./images/v2.jpg"
+import v16 from "./images/v2.jpg"
+import v17 from "./images/v2.jpg"
+import v18 from "./images/v2.jpg"
+import v19 from "./images/v2.jpg"
+import v20 from "./images/v2.jpg"
 
 export const ITEMS = [
     {
         id: 1,
-        itemNames: "potato",
+        itemNames: "Potato",
         price: 19,
         itemImg: veg1,
     },
+
+    {
+        id: 2,
+        itemNames: "Cauliflower",
+        price: 30,
+        itemImg: v2,
+    },
+
+    {
+        id: 3,
+        itemNames: "Onion",
+        price: 40,
+        itemImg: v3,
+    },
+    
+    {
+        id: 4,
+        itemNames: "Bitter Root",
+        price: 23,
+        itemImg: v4,
+    },
+    
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
+    
+    {
+        id: ,
+        itemNames: "",
+        price: ,
+        itemImg: v,
+    },
 ];
+
