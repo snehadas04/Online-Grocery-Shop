3e49d133e692e3d3c076758bd9a6f1de7dc3d941
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-08-25 19:56:19 +0530

    Add files via upload

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <div>
-      N
+    <div className="navbar">
+      <div className="links">
+        <Link to="/"> Shop </Link>
+        <Link to="/cart"> Cart </Link>
+      </div>
     </div>
   )
 };
