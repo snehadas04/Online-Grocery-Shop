f17db5ff786bfc82f4942187b87185510a094909
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-08-26 00:00:10 +0530

    Add files via upload

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -7,5 +7,3 @@ export const Cart = () => {
     </div>
   );
 };
-
-
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 export const Shop = () => {
   return (
-    <div>
-      SHOP
+    <div className='shop'>
     </div>
   );
 };
