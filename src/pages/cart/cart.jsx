71ca47bf7b81f76b98bd51c4d6d6b41c5d32b832
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-08-25 21:33:39 +0530

    Add files via upload

---
+++ src/pages/cart/cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const cart = () => {
+  return (
+    <div>
+      CART
+    </div>
+  )
+}
+
+export default cart
---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const shop = () => {
+  return (
+    <div>
+      SHOP
+    </div>
+  )
+}
+
+export default shop
