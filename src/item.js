b40d87d56fd0a04cda0d349673025c7e40f2b3fb
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-08-26 00:55:18 +0530

    Add files via upload

---
+++ src/item.js
@@ -0,0 +1,10 @@
+import veg1 from "./images/veg.1jp";
+
+export const ITEMS = [
+    {
+        id: 1,
+        itemNames: "potato",
+        price: 19/kg,
+        itemImg: veg1,
+    },
+];
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import{ITEMS} from "../../item";
+import {Items} from "./items";
 
 export const Shop = () => {
   return (
     <div className='shop'>
+      <div className='"shopTitle'>
+        <h1> Vegetable Store</h1>
+      </div>
+      <div className='vegs'> 
+        {ITEMS.map((items) => {
+          <Items />
+        })}
+      </div>
     </div>
   );
 };
