787b5a94c3dc25400d921c8092c876d42c27c7ae
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-08-27 20:21:47 +0530

    Add files via upload

--- src/fruit.js
+++ src/fruit.js
@@ -1,4 +1,12 @@
 import f1 from "./images/f1.jpg";
+import f2 from "./images/f2.jpg";
+import f3 from "./images/f3.jpg";
+import f4 from "./images/f4.jpg";
+import f5 from "./images/f5.jpg";
+import f6 from "./images/f6.jpg";
+import f7 from "./images/f7.jpg";
+import f8 from "./images/f8.jpg";
+
 
 export const FRUIT = [
     {
