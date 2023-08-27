a95852af0c215ff76dcda053a3bee941b11219cc
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-08-27 21:42:00 +0530

    Add files via upload

--- src/nonveg.js
+++ src/nonveg.js
@@ -1,26 +1,32 @@
-import f1 from "./images/f1.jpg";
-import f2 from "./images/f2.jpg";
+import n1 from "./images/n1.jpg";
+import n2 from "./images/n2.jpg";
+import n3 from "./images/n3.jpg";
+import n4 from "./images/n4.jpg";
+import n5 from "./images/n5.jpg";
+import n6 from "./images/n6.jpg";
+import n7 from "./images/n7.jpg";
+import n8 from "./images/n9.jpg";
 
 export const NONVEG = [
     {
-        nid: ,
-        nName: "",
-        nPrice: ,
-        nImg: n,
+        nid: 1,
+        nName: "Cutting Chicken",
+        nPrice: 160,
+        nImg: n1,
     },
 
     {
-        nid: ,
-        nName: "",
-        nPrice: ,
-        nImg: n,
+        nid: 2,
+        nName: "Boneless Chicken",
+        nPrice: 240,
+        nImg: n2,
     },
 
     {
-        nid: ,
-        nName: "",
-        nPrice: ,
-        nImg: n,
+        nid: 3,
+        nName: "Chicken Wings",
+        nPrice: 190,
+        nImg: n3,
     },
 
     {
