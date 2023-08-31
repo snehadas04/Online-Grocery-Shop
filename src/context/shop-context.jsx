1c5f827f8f0ef0a06e573e08952ac34614c1aa3b
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-08-31 23:36:48 +0530

    Add files via upload

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -3,6 +3,7 @@ import React, { createContext } from 'react';
 export const ShopContext = createContext(null);
 
 export const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState();
   return (
     <ShopContext.Provider>(props.product)</ShopContext.Provider>
   );
