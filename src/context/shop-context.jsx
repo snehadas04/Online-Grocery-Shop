2e8a5899fa47d65f64581c5cfdec82392d119bd6
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-09-01 23:40:50 +0530

    Add files via upload

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -7,10 +7,13 @@ const getDefaultCart = () => {
   for(let i=1; i<VEG.length+1; i++){
     cart[i] = 0;
   }
-}
+  return cart;
+};
 
 export const ShopContextProvider = (props) => {
-  const [cartItems, setCartItems] = useState();
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+
+  const addToCart = (itemId) =>{}
   return (
     <ShopContext.Provider>(props.product)</ShopContext.Provider>
   );
